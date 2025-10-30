d80bf630c748e61b54f818039f6f1288c52a7fb7
benvarzam <benje1612@gmail.com>
2025-10-30 20:28:16 +0100

    Todo funciona perfectamente

--- src/Mayusculas.java
+++ src/Mayusculas.java
@@ -6,25 +6,30 @@ public class Mayusculas {
         try {
             System.out.println("Introduce el texto para convertirlo en mayusculas");
             Scanner teclado = new Scanner(System.in);
-            String texto = teclado.nextLine();
+            String texto, linea;
             ProcessBuilder pb = new ProcessBuilder("java", "src/Hijo.java");
             pb.redirectErrorStream(true);
-            Process hijo = pb.start();
+            Process hijo;
 
-            OutputStream os = hijo.getOutputStream();
-            PrintWriter pw = new PrintWriter(os,true);
-
-            InputStream is = hijo.getInputStream();
-            pw.println(is);
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
+            OutputStream os;
+            PrintWriter pw;
+            InputStream is;
+            BufferedReader br;
+            do {
+                hijo = pb.start();
+                os = hijo.getOutputStream();
+                pw = new PrintWriter(os,true);
+                is = hijo.getInputStream();
+                br = new BufferedReader(new InputStreamReader(is));
+            texto = teclado.nextLine();
             pw.println(texto);
             pw.flush();
-
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                System.out.println("Desde el hijo: " + linea);
+            if(!texto.equals("fin")){
+                while ((linea = br.readLine()) != null) {
+                    System.out.println("Desde el hijo: " + linea);
+                }
             }
+            } while (!texto.equals("fin"));
             hijo.waitFor();
         }catch (Exception e){
             System.err.println(e.getMessage());
